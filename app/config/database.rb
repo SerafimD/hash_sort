2ec2e8ba35f537ae3c69d2709925bae9868022e9
Dmitry <avalonsdm@gmail.com>
2018-11-30 10:52:24 +0300

    fix

--- app/config/database.rb
+++ app/config/database.rb
@@ -1,19 +1,8 @@
 require 'active_record'
-if ENV['RACK_ENV'].eql?('development') || ENV['RACK_ENV'].eql?('test')
-  database_name = "hash_#{ENV['RACK_ENV']}"
-  db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
-  ActiveRecord::Base.establish_connection(
+database_name = "hash_#{ENV['RACK_ENV']}"
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+ActiveRecord::Base.establish_connection(
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :database => "#{database_name}",
     :encoding => 'utf8'
-  )
-else
-  ActiveRecord::Base.establish_connection(
-    :adapter => 'postgresql',
-    :database => 'app-dev',
-    :encoding => 'utf8',
-    :username => 'user',
-    :host => 'localhost',
-    :passwod => ENV['MYAPP_DATABASE_PASSWORD']
-  )
-end
+)
