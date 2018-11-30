dbab9e693bc76fbf39e296205a0209fafc3a9707
Dmitry <avalonsdm@gmail.com>
2018-11-30 12:15:08 +0300

    done

--- app/config/database.rb
+++ app/config/database.rb
@@ -1,8 +1,8 @@
-# require 'active_record'
-# database_name = "hash_#{ENV['RACK_ENV']}"
-# db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
-# ActiveRecord::Base.establish_connection(
-#     :adapter => 'postgresql',
-#     :database => "#{database_name}",
-#     :encoding => 'utf8'
-# )
+require 'active_record'
+database_name = "hash_#{ENV['RACK_ENV']}"
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :database => "#{database_name}",
+    :encoding => 'utf8'
+)
